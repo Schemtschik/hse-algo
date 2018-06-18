13595f472118b37daf7bea73365dce53636d0320
Yevgeny Shemchik <schemtschik@gmail.com>
2018-06-18 15:03:41 +0300

    Review fixes

--- min-cost-max-flow.cpp
+++ min-cost-max-flow.cpp
@@ -17,162 +17,183 @@
 #include <utility>
 #include <cassert>
 
-using namespace std;
+struct Solution {
 
-typedef long long DistType;
+    typedef long long DistType;
 
-const int N = 202;
-const DistType INF = (DistType)1e18 + 7ll;
+    struct Edge;
 
-DistType p[N], d[N];
+    static const int N = 202;
+    const DistType INF = (DistType) 1e18 + 7ll;
 
-struct Edge;
+    DistType previous[N], dist[N];
 
-struct Edge {
-    int id;
-    int f, t;
-    DistType w;
-    int flow = 0, cap = 1;
-    Edge* paired;
+    Edge* previousEdge[N];
+    std::vector<Edge*> edges;
+    std::vector<std::pair<int, Edge*>> graph[N];
 
-    inline bool able() {
-        return (flow < cap);
+    size_t s, t;
+
+    struct Edge {
+        Solution* solution;
+        size_t id;
+        size_t from, to;
+        DistType weight;
+        int flow = 0, capacity = 1;
+        Edge* paired;
+
+        inline bool able() {
+            return (flow < capacity);
+        }
+
+        inline DistType getWeight() {
+            return (weight + solution->previous[from] - solution->previous[to]);
+        }
+
+        inline Edge getReversed() {
+            Edge res = (*this);
+            std::swap(res.from, res.to);
+            res.capacity = 0;
+            res.weight = -weight;
+            return res;
+        }
+
+        inline void pushFlow() {
+            flow++;
+            paired->flow--;
+        }
+    };
+
+    inline void dijkstra() {
+        dist[s] = 0;
+        for (size_t i = 0; i < s; i++)
+            dist[i] = INF;
+
+        std::set<std::pair<DistType, int>> q;
+        q.insert({dist[s], s});
+        while (!q.empty()) {
+            int v = q.begin()->second;
+            q.erase(q.begin());
+            for (auto &u : graph[v]) {
+                if (u.second->able() && dist[v] + u.second->getWeight() < dist[u.second->to]) {
+                    q.erase({dist[u.second->to], u.second->to});
+                    dist[u.second->to] = dist[v] + u.second->getWeight();
+                    previousEdge[u.second->to] = u.second;
+                    q.insert({dist[u.second->to], u.second->to});
+                }
+            }
+        }
+
+        for (size_t i = 0; i < s; i++)
+            previous[i] += dist[i];
     }
 
-    inline DistType getWeight() {
-        return (w + p[f] - p[t]);
+    inline void addEdge(Edge* edge) {
+        edge->solution = this;
+        edges.push_back(edge);
+        graph[edge->from].emplace_back(edge->to, edge);
+        Edge *rev = new Edge(edge->getReversed());
+        rev->paired = edge;
+        edge->paired = rev;
+        edges.push_back(rev);
+        graph[edge->to].emplace_back(edge->from, rev);
     }
 
-    inline Edge rev() {
-        Edge res = (*this);
-        swap(res.f, res.t);
-        res.cap = 0;
-        res.w = -w;
-        return res;
+    size_t n, m, k;
+
+    void read() {
+        std::cin >> n >> m >> k;
+        for (size_t i = 1; i <= m; i++) {
+            Edge* edge = new Edge();
+            edge->id = i;
+            std::cin >> edge->from >> edge->to >> edge->weight, edge->from--, edge->to--;
+            addEdge(edge);
+            Edge* rev = new Edge(*edge);
+            std::swap(rev->from, rev->to);
+            addEdge(rev);
+        }
+
+        s = n;
+        t = n - 1;
+        Edge* edge = new Edge();
+        edge->from = n;
+        edge->to = 0;
+        edge->weight = 0;
+        edge->capacity = (int)k;
+        addEdge(edge);
     }
 
-    inline void pushFlow() {
-        flow++;
-        paired->flow--;
+    bool solve(DistType& cost, std::vector<std::vector<int>>& ans) {
+        memset(previous, 0, sizeof(previous));
+        dijkstra();
+
+        if (dist[t] == INF)
+            return false;
+
+        cost = 0;
+        size_t cnt = 0;
+        do {
+            cnt++;
+            size_t cur = t;
+            while (cur != s) {
+                previousEdge[cur]->pushFlow();
+                cur = previousEdge[cur]->from;
+            }
+            dijkstra();
+        } while (dist[t] != INF);
+
+        if (cnt < k)
+            return false;
+
+        ans.resize(k);
+        std::vector<std::pair<int, int>> tmp[N];
+        for (auto i : edges)
+            if (i->flow > 0) {
+                tmp[i->to].emplace_back(i->from, i->id);
+                cost += (i->flow) * (i->weight);
+            }
+
+        for (size_t i = 0; i < k; i++) {
+            size_t cur = t;
+            while (cur != 0) {
+                std::pair<size_t , int> v(tmp[cur][(int)tmp[cur].size() - 1]);
+                tmp[cur].pop_back();
+                ans[i].push_back(v.second);
+                cur = v.first;
+            }
+        }
+        return true;
+    }
+
+    void write(DistType cost, std::vector<std::vector<int>>& ans) {
+        std::cout.precision(6);
+        std::cout << std::fixed << (double) cost / (double) k << std::endl;
+        for (auto &i : ans) {
+            reverse(i.begin(), i.end());
+            std::cout << i.size() << ' ';
+            for (auto &j : i)
+                std::cout << j << ' ';
+            std::cout << std::endl;
+        }
+    }
+
+    void writeNoSolution() {
+        std::cout << -1 << std::endl;
     }
 };
 
-Edge* e[N];
-vector<Edge*> edges;
-vector<pair<int, Edge*>> g[N];
-
-int s, t;
-
-inline void dijkstra() {
-    d[s] = 0;
-    for (int i = 0; i < s; i++)
-        d[i] = INF;
-
-    set<pair<DistType, int>> q;
-    q.insert({d[s], s});
-    while (!q.empty()) {
-        int v = q.begin()->second;
-        q.erase(q.begin());
-        for (auto& u : g[v]) {
-            if (u.second->able() && d[v] + u.second->getWeight() < d[u.second->t]) {
-                q.erase({d[u.second->t], u.second->t});
-                d[u.second->t] = d[v] + u.second->getWeight();
-                e[u.second->t] = u.second;
-                q.insert({d[u.second->t], u.second->t});
-            }
-        }
-    }
-
-    for (int i = 0; i < s; i++)
-        p[i] += d[i];
-}
-
-inline void addEdge(Edge* edge) {
-    edges.push_back(edge);
-    g[edge->f].emplace_back(edge->t, edge);
-    Edge* rev = new Edge(edge->rev());
-    rev->paired = edge;
-    edge->paired = rev;
-    edges.push_back(rev);
-    g[edge->t].emplace_back(edge->f, rev);
-}
-
 int main() {
-    ios_base::sync_with_stdio(false);
+    std::ios_base::sync_with_stdio(false);
 
-    int n, m, k;
-    cin >> n >> m >> k;
-    for (int i = 1; i <= m; i++) {
-        Edge* edge = new Edge();
-        edge->id = i;
-        cin >> edge->f >> edge->t >> edge->w, edge->f--, edge->t--;
-        addEdge(edge);
-        Edge* rev = new Edge(*edge);
-        swap(rev->f, rev->t);
-        addEdge(rev);
-    }
-
-    s = n;
-    t = n - 1;
-    Edge* edge = new Edge();
-    edge->f = n;
-    edge->t = 0;
-    edge->w = 0;
-    edge->cap = k;
-    addEdge(edge);
-
-    memset(p, 0, sizeof(p));
-    dijkstra();
-
-    if (d[t] == INF) {
-        cout << -1 << endl;
-        return 0;
-    }
-
-    DistType cost = 0;
-    int cnt = 0;
-    do {
-        cnt++;
-        int cur = t;
-        while (cur != s) {
-            e[cur]->pushFlow();
-            cur = e[cur]->f;
-        }
-        dijkstra();
-    } while (d[t] != INF);
-
-    if (cnt < k) {
-        cout << -1 << endl;
-        return 0;
-    }
-
-    vector<vector<int>> ans(k);
-    vector<pair<int, int>> tmp[N];
-    for (auto i : edges)
-        if (i->flow > 0) {
-            tmp[i->t].emplace_back(i->f, i->id);
-            cost += (i->flow) * (i->w);
-        }
-
-    for (int i = 0; i < k; i++) {
-        int cur = t;
-        while (cur != 0) {
-            pair<int, int> v(tmp[cur][(int)tmp[cur].size() - 1]);
-            tmp[cur].pop_back();
-            ans[i].push_back(v.second);
-            cur = v.first;
-        }
-    }
-
-    cout.precision(6);
-    cout << fixed << (double)cost / (double)k << endl;
-    for (auto& i : ans) {
-        reverse(i.begin(), i.end());
-        cout << i.size() << ' ';
-        for (auto& j : i)
-            cout << j << ' ';
-        cout << endl;
-    }
+    Solution solution;
+    solution.read();
+    Solution::DistType cost = 0;
+    std::vector<std::vector<int>> ans;
+    if (solution.solve(cost, ans))
+        solution.write(cost, ans);
+    else
+        solution.writeNoSolution();
 
     return 0;
 }
+
